c0adcdc4e581a5a60cb73ec10fa0f6404d770062
vchychy <vchychyrko@anaconda.com>
2026-01-23 20:48:20 +0200

    fix: glib->libglib; rm gettext osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,10 +8,13 @@ cd build
 rm ../docs/reference/meson.build
 touch ../docs/reference/meson.build 
 
-# necessary to ensure the gobject-introspection-1.0 pkg-config file gets found
-# meson needs this to determine where the g-ir-scanner script is located
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig
-export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
+# # necessary to ensure the gobject-introspection-1.0 pkg-config file gets found
+# # meson needs this to determine where the g-ir-scanner script is located
+# export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig
+# export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
+
+export PKG_CONFIG="${BUILD_PREFIX}/bin/pkg-config"
+export PKG_CONFIG_PATH="${BUILD_PREFIX}/lib/pkgconfig:${BUILD_PREFIX}/share/pkgconfig:${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig:${PKG_CONFIG_PATH:-}"
 
 echo "MESON_ARGS=${MESON_ARGS}"
 
